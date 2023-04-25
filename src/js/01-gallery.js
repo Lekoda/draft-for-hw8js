23bb6c1407d833f5a701772db691acfeef900f31
Valeria Otchenash <kratorchestra@gmail.com>
2023-04-25 11:31:25 +0300

    adds src

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryBox = document.querySelector('.gallery');
 
+(function () {
+    const markUp = galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+    galleryBox.insertAdjacentHTML('beforeend', markUp);
+})()
+
+galleryBox.addEventListener('click', getPreview)
+
+function getPreview(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+    {
+    onShow: () => 
+        window.addEventListener('keydown', onEscKeyPress),
+    onClose: () => 
+        window.removeEventListener('keydown', onEscKeyPress),  
+}
+)
+function onEscKeyPress(evt) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = ESC_KEY_CODE;
+    if (isEscKey) {
+    instance.close();
+    }
+}
+instance.show();
+}
 console.log(galleryItems);
+
